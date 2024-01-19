713ddaee2fb175dd23ec858da57bd2bfe4b80555
Taylor Lo <taylorloph@gmail.com>
2024-01-19 11:59:18 -0500

    02 - Structures

---
+++ 02 - Structures/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+struct Rectangle
+{
+    int length;
+    int width;
+    char x;
+};
+
+int main()
+{
+    struct Rectangle r1={10,5};
+
+    r1.length=15;
+    r1.width=7;
+
+    cout<<r1.length<<endl;
+    cout<<r1.width<<endl;
+
+    return 0;
+}
